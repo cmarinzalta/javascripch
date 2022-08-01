536230eb2d3bd6e843a3d18e5986e784fd70cc6a
Candela Marinzalta <candela.marinzalta@gmail.com>
2022-08-01 14:55:17 -0300

    Cambios en el html

--- functions.js
+++ functions.js
@@ -1,52 +1,6 @@
 function probarCodigo()
     { 
 
-
-//ENTREGA OPCIONAL CODERHOUSE. CANDELA MARINZALTA
-
-/*
-let producto
-let precio
-
-producto = prompt("Agregue un producto a su carrito") 
-precio = 0
-
-while(producto != ""){
-  switch (producto){
-
-    case "Yerba" : 
-      alert (("El total de su carrito es $") + (precio += 500))
-      break ;
-
-    case "Leche" : 
-      alert (("El total de su carrito es $") + (precio += 200))
-      break ;
-
-    case "Azucar" : 
-      alert (("El total de su carrito es $") + (precio += 150))
-      break ;
-
-    case "Sal" : 
-      alert (("El total de su carrito es $") + (precio += 80))
-      break ;
-
-    default :
-      alert ("Ups! no tenemos ese producto")
-      break;
-  }
-  producto = prompt("Agregue un producto a su carrito")
-}
-
- if (precio > 500) {
-    alert(("Genial! Conseguiste un descuento del 20%" + "\n" + "El precio final de tu compra es de $") + (precio - precio*0.2))
-  } else {
-     alert (("Finalizaste tu compra." + "\n" + "Total a pagar $") + (precio))
-    }
-
-*/
-
-
-
 // ENTREGA DE SIMULADOR INTERACTIVO
 
 //Creamos un quizz para pilotos, hay tres categorias Historia de la aviacion, pilotos principiantes y pilotos avanzados. Elegis una categoria y empieza la experiencia. Por cada respuesta correcta sumaras dos puntos y por cada incorrecta restas 1. 
