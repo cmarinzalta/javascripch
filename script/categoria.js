b5fa8d5b34a40c8a3fd138b2c68a1a3827407157
Candela Marinzalta <candela.marinzalta@gmail.com>
2022-09-19 12:00:07 -0300

    ultimo cambio

---
+++ script/categoria.js
@@ -0,0 +1,184 @@
+// class quizz {
+//     constructor(categoria, pregunta, respuestaCorrecta, respuestaIncorrectaUno, respuestaIncorrectaDos) {
+//         this.categoria = categoria
+//         this.pregunta = pregunta;
+//         this.respuestaCorrecta = respuestaCorrecta;
+//         this.respuestaIncorrectaUno = respuestaIncorrectaUno;
+//         this.respuestaIncorrectaDos = respuestaIncorrectaDos;
+//     }
+// }
+
+// export const trivia = [
+//     {
+//         categoria: "Historia De Aviacion",
+//         correctas: [
+//             "39.000",
+//             "1903",
+//             "1988",
+//             "1947",
+//             "Amelia Earhart"
+//         ],
+//         quizz: [
+//             {
+//                 id: 1,
+//                 pregunta: "¿Cuantos aviones militares y comerciales hay en el mundo?",
+//                 respuestas: [
+//                     "39.000",
+//                     "26.000",
+//                     "42.000"
+//                 ]
+//             },
+//             {
+//                 id: 2,
+//                 pregunta: "Los hermanos Wright crearon el primer avion. ¿En que año fue?",
+//                 respuestas: [
+//                     "1903",
+//                     "1910",
+//                     "1900"
+//                 ]
+//             },
+//             {
+//                 id: 3,
+//                 pregunta: "El Antonov An-225 fue el avion mas grande del mundo ¿En que año se registro su primer vuelo",
+//                 respuestas: [
+//                     "1988",
+//                     "1989",
+//                     "1998"
+//                 ]
+//             },
+//             {
+//                 id: 4,
+//                 pregunta: "El I.Ae. 27 Pulqui fue un avión a reacción diseñado y construido en Argentina ¿En que año?",
+//                 respuestas: [
+//                     "1947",
+//                     "1950",
+//                     "1949"
+//                 ]
+//             },
+//             {
+//                 id: 5,
+//                 pregunta: "¿Como se llamaba la primer mujer piloto?",
+//                 respuestas: [
+//                     "Amelia Earhart",
+//                     "Raymonde de Laroche",
+//                     "Emma Catalina Encinas Aguayo"
+//                 ]
+//             }
+//         ]
+//     },
+//     {
+//         categoria: "Piloto Principiante",
+//         correctas: [
+//             "Medir la altura",
+//             "Timon",
+//             "Disminuye la presión encima del ala",
+//             "Ángulo de ataque",
+//             "Cabeceo"
+//         ],
+//         quizz: [
+//             {
+//                 id: 1,
+//                 pregunta: "¿Para que sirve el altimetro?",
+//                 respuestas: [
+//                     "Medir la altura",
+//                     "Reducir la presion del aire en la altura",
+//                     "Reducir el arrastre"
+//                 ]
+//             },
+//             {
+//                 id: 2,
+//                 pregunta: "¿Qué herramienta usan los pilotos para controlar los giros en el eje vertical?",
+//                 respuestas: [
+//                     "Timon",
+//                     "Fuselaje",
+//                     "Alas"
+//                 ]
+//             },
+//             {
+//                 id: 3,
+//                 pregunta: "¿A qué ayuda la curva en la parte superior del ala de un avión?",
+//                 respuestas: [
+//                     "Disminuye la presión encima del ala",
+//                     "Ofrece más espacio para misiles",
+//                     "Controla el bandazo"
+//                 ]
+//             },
+//             {
+//                 id: 4,
+//                 pregunta: "Si el piloto levanta la nariz del avión, ¿qué es lo que está cambiando?",
+//                 respuestas: [
+//                     "Ángulo de ataque",
+//                     "Cabeceo",
+//                     "Gravedad específica"
+//                 ]
+//             },
+//             {
+//                 id: 5,
+//                 pregunta: "¿Qué elemento controlan los timones de profundidad?",
+//                 respuestas: [
+//                     "Cabeceo",
+//                     "Radio",
+//                     "Vortice"
+//                 ]
+//             }
+//         ]
+//     },
+//     {
+//         categoria: "Piloto Avanzado",
+//         correctas: [
+//             "Para hacer un aterrizaje instrumental",
+//             "Indica la velocidad apta para vuelos con FLAPS extendidos",
+//             "Frecuencia muy alta de rango omnidireccional",
+//             "Instrumento para ser localizado por el radar",
+//             "Señales de frecuencia de amplitud modulada"
+//         ],
+//         quizz: [
+//             {
+//                 id: 1,
+//                 pregunta: "¿Para que sirve el ILS?",
+//                 respuestas: [
+//                     "Para hacer un aterrizaje instrumental",
+//                     "Para localizar el aeropuerto",
+//                     "Para cogerregir el giro direccional"
+//                 ]
+//             },
+//             {
+//                 id: 2,
+//                 pregunta: "¿Que indica el arco blanco en el velocimetro?",
+//                 respuestas: [
+//                     "Indica la velocidad apta para vuelos con FLAPS extendidos",
+//                     "Indica la velocidad de nunca exceder",
+//                     "Indica la velocidad de maniobra"
+//                 ]
+//             },
+//             {
+//                 id: 3,
+//                 pregunta: "¿Que es el VOR?",
+//                 respuestas: [
+//                     "Frecuencia muy alta de rango omnidireccional",
+//                     "Sistema de posicion global",
+//                     "Sistema de aterrizaje instrumental"
+//                 ]
+//             },
+//             {
+//                 id: 4,
+//                 pregunta: "¿Que es el transponder?",
+//                 respuestas: [
+//                     "Instrumento para ser localizado por el radar",
+//                     "Sistema de posicion global",
+//                     "Sistema para comunicacion radiofonica"
+//                 ]
+//             },
+//             {
+//                 id: 5,
+//                 pregunta: "¿Que detecta el ADF?",
+//                 respuestas: [
+//                     "Señales de frecuencia de amplitud modulada",
+//                     "Señales de radio de frecuencia modulada",
+//                     "Frecuencias del radar del aeropuerto"
+//                 ]
+//             }
+//         ]
+//     }
+// ]
+
---
+++ script/inicio.js
@@ -0,0 +1,43 @@
+let inicioDeSesion = document.querySelector('#btnInicioSesion')
+inicioDeSesion.addEventListener("click", Ingresar)
+function Ingresar() {
+
+    let emailIngresado = document.querySelector('#ingreseEmail').value;
+    let claveIngresada = document.querySelector('#ingreseClave').value;
+    let textoInicio = document.querySelector('#inicioSesion');
+    let mensaje;
+
+    let listaDeUs = LlamarUs();
+    let retornarUs = listaDeUs.find((usuario) => usuario.email == emailIngresado)
+    if (retornarUs) {
+        if (retornarUs.clave == claveIngresada) {
+            mensaje = `Bienvenido ${emailIngresado}`
+            textoInicio.innerText = mensaje
+
+        } else {
+            mensaje = "Ups! Algo salio mal"
+            textoInicio.innerText = mensaje
+        }
+    } else {
+        mensaje = "Ups! Algo salio mal"
+        textoInicio.innerText = mensaje
+    }
+
+}
+
+function LlamarUs() {
+    let usuario = [];
+    usuario.push(new Usuario("pepe@g.com", "111"));
+    usuario.push(new Usuario("pepa@g.com", "112"));
+    usuario.push(new Usuario("pepi@g.com", "113"));
+    usuario.push(new Usuario("pepu@g.com", "114"));
+    usuario.push(new Usuario("pepo@g.com", "115"));
+    return usuario;
+}
+
+class Usuario {
+    constructor(email, clave) {
+        this.email = email;
+        this.clave = clave;
+    }
+}
